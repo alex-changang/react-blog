c4c8f02a07a8c6b0fa2600ed99a82e27299f5881
alex-changang <37599124+alex-changang@users.noreply.github.com>
2020-02-11 14:44:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Node.js Express backend
+To run, enter:
+npx babel-node src/server.js
+npx nodemon --exec npx babel-node src/server.js
---
+++ src/build/precache-manifest.e21754508d8ac02e36ba07f98be667d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0474fff66c525254384e4dc1e1bba6c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adaebd9a268ab492944",
+    "url": "/static/css/main.2a20facb.chunk.css"
+  },
+  {
+    "revision": "6f10e7fe4aee24219ed9",
+    "url": "/static/js/2.4a7b617f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.4a7b617f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8adaebd9a268ab492944",
+    "url": "/static/js/main.32c4aed3.chunk.js"
+  },
+  {
+    "revision": "b7fe6da259cdd2d93ed2",
+    "url": "/static/js/runtime-main.72e53517.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
